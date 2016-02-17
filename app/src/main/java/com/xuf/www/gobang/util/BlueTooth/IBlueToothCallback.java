7acf4ab432835aff7f85e376a9d5363d0ebb595d
uin3566 <897445793@qq.com>
2016-02-17 23:09:00 +0800

    BlueToothWrapper,封装蓝牙被发现以及查找设备

--- app/src/main/java/com/xuf/www/gobang/util/BlueTooth/IBlueToothCallback.java
+++
@@ -1,9 +0,0 @@
-package com.xuf.www.gobang.util.BlueTooth;
-
-/**
- * Created by lenov0 on 2016/2/16.
- */
-public interface IBlueToothCallback {
-    public void onSuccess();
-    public void onFail();
-}
